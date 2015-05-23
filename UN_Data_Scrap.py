295c4bb74e7e8aa5fa22d3d77b0202cfeb09edcb
ryanarbow <ryanarbow@gmail.com>
2015-05-22 21:04:19 -0500

    Ready to loop

--- UN_Data_Scrap.py
+++ UN_Data_Scrap.py
@@ -8,20 +8,22 @@ import requests
 
 #Import page
 url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
-r = requests.get(url)
 
+
+r = requests.get(url)
 #Pass to Beautiful Soup
 soup = BeautifulSoup(r.content)
 
 #Return tags
-#soup.find_all("tr", class_="tcont")
-rows = soup.find_all('tr', {'class': 'tcont'})
-
-r = rows[0]
-
-r.find_all('td')
-
-columns = r.find_all('td')
+table = soup.find_all('table')[6].find_all('tr', {'class': 'tcont'})
 
 #Need to pull columns 0,1,4,7,10 from each row
+for rows in table:
+    col = rows.find_all('td')
+    country = col[0].string
+    year = col[1].string
+    total = col[4].string
+    men = col[7].string
+    women = col[10].string
+    
 
