26b492f5f99720079fd92714da82d70a52f618f5
ryanarbow <ryanarbow@gmail.com>
2015-05-25 11:59:57 -0500

    Almost done

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,6 +1,8 @@
 #Create a new file called 'logistic_regression.py'. For this lesson, we're going to need pandas and statsmodels.
 import pandas as pd
-imports statsmodels.api as sm
+import statsmodels.api as sm
+import math
+
 
 #Load the data
 loansData = pd.read_csv('loansData_clean.csv')
@@ -22,3 +24,27 @@ loansData['Intercept'] = intercept
 
 #Create a list of the column names of our independent variables, including the intercept, and call it ind_vars.
 ind_vars = ['Intercept','Amount.Requested','FICO.Score']
+
+df = loansData
+
+#Define the logistic regression model.
+logit = sm.Logit(df['IR_TF'], df[ind_vars])
+
+#Fit the model
+result = logit.fit()
+
+#Get the fitted coefficients from the results.
+coeff = result.params
+print coeff
+
+#Write a function called logistic_function that will take a FICO Score and a Loan Amount 
+#of this linear predictor, and return p. (Try not to hardcode any values if you can! 
+#Hint: pass the coefficients object to the function as an argument.)
+def logistic_function(FicoScore, LoanAmount):
+	# p(x) = 1/(1 + e^(intercept + 0.087423(FicoScore) − 0.000174(LoanAmount))
+	prob = 1/(1 + math.exp(coeff[0] + coeff[2]*FicoScore+coeff[1]*LoanAmount))
+	if prob > 0.7:
+		p = 1
+	else:
+		p = 0
+	return prob, p
