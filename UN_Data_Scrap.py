9a0b67f1dff0f348d63bee916021a2b5e3a9bb1e
ryanarbow <ryanarbow@gmail.com>
2015-05-26 21:55:24 -0500

    Table included

--- UN_Data_Scrap.py
+++ UN_Data_Scrap.py
@@ -10,19 +10,18 @@ import pandas as pd
 #Import page
 url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
 
-
 r = requests.get(url)
+
 #Pass to Beautiful Soup
 soup = BeautifulSoup(r.content)
 
 #Return tags
-table = soup.find_all('table')[6].find_all('tr', {'class': 'tcont'})
-table2 = [x for x in table if len(x)== 25]
+table = soup.find_all('table')[6]
+table1 = table.find_all('table')[1].find_all('tr', {'class': 'tcont'})
 
 #Need to pull info from columns 0,1,4,7,10 from each row
-
 records = []
-for rows in table2:
+for rows in table1:
     col = rows.find_all('td')
     country = col[0].string
     year = col[1].string
@@ -31,5 +30,5 @@ for rows in table2:
     women = col[10].string
     record = (country, year, total, men, women)
     records.append(record)
-column_name = ['country', 'year', 'total', 'men', 'women']
-table = pd.DataFrame(records, columns = column_name )
+column_name = ['country', 'year', 'total', 'men', 'women']   
+df = pd.DataFrame(records, columns = column_name)
