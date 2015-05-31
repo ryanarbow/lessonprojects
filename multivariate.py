f525ec475a3d79cd939d2b8c733067c89c8d4730
ryanarbow <ryanarbow@gmail.com>
2015-05-31 13:13:55 -0500

    Added homeownership

--- multivariate.py
+++ multivariate.py
@@ -1,3 +1,5 @@
+cd /Users/Ryan/Desktop/Thinkful
+
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
@@ -12,28 +14,14 @@ loanStats['int_rate'] = loanStats['int_rate'].map(lambda x: round(float(x.rstrip
 loanStats['home_ownership_ord'] = pd.Categorical(loanStats.home_ownership).labels
 
 #Extract columns
-intrate = loanStats['int_rate']
-annualinc = loanStats['annual_inc']
-homeown = loanStats['home_ownership_ord']
-
-#Use income (annual_inc) to model interest rates (int_rate)
-# The dependent variable
-y = np.matrix(intrate).transpose()
-
-# The independent variables shaped as columns
-x1 = np.matrix(annualinc).transpose()
-
-X = sm.add_constant(x1)
-model = sm.OLS(y, X)
-f = model.fit()
-f.summary()
-
-#Add home ownership (home_ownership) to the model.
-x2 = np.matrix(homeown).transpose()
-
-x = np.column_stack([x1,x2])
+X = loanStats['int_rate']
+y = loanStats['annual_inc']
 
 X = sm.add_constant(X)
-model = sm.OLS(y, X2)
-f = model.fit()
-f.summary()
+est = sm.OLS(y, X).fit()
+est.summary()
+
+X1 = loanStats[['int_rate', 'home_ownership_ord']]
+X2 = sm.add_constant(X1)
+est = sm.OLS(y, X2).fit()
+est.summary()
